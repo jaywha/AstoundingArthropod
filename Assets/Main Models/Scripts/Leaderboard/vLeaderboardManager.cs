5dbc2533db4868b1bf6218e3c522cac44789c295
jaywha <jayw685@gmail.com>
2018-10-09 22:51:35 -0500

    Added moving platform logic and updated web template.
    Used the base model transformations to move a platform between 2 cubes that act as positions. Also using dreamlo to manage a leaderboard, although further testing is necessary.

--- Assets/Main Models/Scripts/Leaderboard/vLeaderboardManager.cs
+++ Assets/Main Models/Scripts/Leaderboard/vLeaderboardManager.cs
@@ -1,34 +1,27 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using UnityEngine;
 
-public class vLeaderboardManager : MonoBehaviour {
+public static class vLeaderboardManager {
 
     private static string LeaderboardURL = "http://dreamlo.com/lb/S015VoJJK0axVX8tW-3MbQ_14WDzqYmEaAUmi3x1GH8Q";
     public static string PlayerName = "Arthropod";
-
-    // Use this for initialization
-    void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 
     /// <summary>
     /// 
     /// </summary>
     /// <remarks>Format: add/NAME:Carmine/SCORE:1000/TIME:90 </remarks>
-    public static void AddScore()
+    public static IEnumerator AddScore()
     {
         var score = vTrackingTimer.times.Aggregate((curr, next) => 100 - (curr + next) % 100);
-        using (var web = new WWW(LeaderboardURL+"/add/"+PlayerName+"/"+score+"/"+vTrackingTimer.FormatTime(score)+"/"))
-        {
-            Debug.Log("Total times added: "+score);
-            Debug.Log("[WWW Response] --> "+Encoding.UTF8.GetString(web.bytes));
-        }
+        var web = new WWW(LeaderboardURL + "/add/" + PlayerName + "/" + score + "/" + vTrackingTimer.FormatTime(score) + "/");
+        
+        Debug.Log("Total times added: "+score);
+        yield return web;
+        Debug.Log("[WWW Response] --> " + Encoding.UTF8.GetString(web.bytes));
     }
 }
